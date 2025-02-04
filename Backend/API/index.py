886e9c1c9b1dd52042ae832399d71eab2db2d6e3
Zer0Dev-exe <igorlizaso16@gmail.com>
2025-02-04 10:43:01 +0100

    update API

--- Backend/API/index.py
+++ Backend/API/index.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI as fa
+from pydantic import BaseModel as bm
+from typing import Optional as op
 
 app = fa()
 
+class Recetas(bm):
+    nombre: str
+    ingredientes: str
+    preparacion: str
+    valoracion: op[float]
+
 @app.get("/")
 def index():
-    return "Hola"
+    return {"message": "Hello, World!"}
+
+@app.get("/recetas/{id}")
+def get_receta(id : int):
+    return {"data": id}
+
+@app.post("/recetas/")
+def crear_receta(receta: Recetas):
+    return {"message": f"Receta creada: {receta.nombre}"}
+
