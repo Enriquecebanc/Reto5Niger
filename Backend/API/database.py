8fd2dc8d971cd52d8a85bdf85c1a7ec3e47fe9c6
Gari <garikworkaddress@gmail.com>
2025-02-10 13:51:52 +0100

    cambios a database.py con tal de dar un poco la base de como se conectara el programa con la base de datos.

--- Backend/API/database.py
+++ Backend/API/database.py
@@ -1,1 +1,16 @@
-recetas_db = []
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        database="reto5"
+    )
+
+def get_db():
+    db = get_db_connection()
+    try:
+        yield db
+    finally:
+        db.close()
