e6a3fa4a102beb6749b5dbf78f3351bddf8f4fb5
Zer0Dev-exe <igorlizaso16@gmail.com>
2025-02-10 14:01:27 +0100

    Ni idea porque estaba esto

--- Frontend/nigerweb/src/pages/registro/registro.js
+++ Frontend/nigerweb/src/pages/registro/registro.js
@@ -142,7 +142,7 @@ const Registro = () => {
                         placeholder="Respuesta"
                         required
                     />
-                </div>twfx
+                </div>
                 <div className="form-group">
                     <ReCAPTCHA
                         sitekey="6Ld3qNIqAAAAAPxKh6xwjGu5eTBOLTweubWkbqLA" // Reemplaza con tu nueva clave de sitio de reCAPTCHA
