b8f243018f7be135df859bdfdb5ab3866dfae461
Gari <garikworkaddress@gmail.com>
2025-02-25 09:07:39 +0100

    oops

--- Backend/API/rutas/__init__.py
+++ Backend/API/rutas/__init__.py
@@ -3,3 +3,4 @@ from .categorias import router as categorias_router
 from .comentarios import router as comentarios_router
 from .ingredientes import router as ingredientes_router
 from .usuarios import router as usuarios_router
+from .cantidades import router as cantidades_router
