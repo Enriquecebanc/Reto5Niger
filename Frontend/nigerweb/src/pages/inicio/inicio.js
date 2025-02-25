aaf2ec58465be9987b8d97dc04fc18c3e34f13d4
Zer0Dev-exe <igorlizaso16@gmail.com>
2025-02-25 09:34:49 +0100

    inicio falta la parte de ingredientes buscador y ir a cada categoria

--- Frontend/nigerweb/src/pages/inicio/inicio.js
+++ Frontend/nigerweb/src/pages/inicio/inicio.js
@@ -1,71 +1,64 @@
-import React, { useState, useEffect } from 'react'; // Importa useEffect
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import axios from 'axios';
 import './inicio.css';
 import logoImage from '../../images/NigerLogo.jpg';
 import '../../App.js';
 import Tenedor from '../../iconos/Tenedor.png';
 import Cuchillo from '../../iconos/Cuchillo.png';
 
-// Simulación de recetas (esto debería venir de una API o base de datos)
-const allRecipes = [
-    { id: 1, name: 'Tarta de Queso', description: 'Una deliciosa tarta de queso cremosa.', categoria: 'Postres', path: '/postres' },
-    { id: 2, name: 'Brownie', description: 'Un bizcocho de chocolate denso y delicioso.', categoria: 'Postres', path: '/postres' },
-    { id: 3, name: 'Flan', description: 'Un postre clásico de huevo y caramelo.', categoria: 'Postres', path: '/postres' },
-    { id: 4, name: 'Espaguetis con Tomate', description: 'Un plato clásico de espaguetis con una salsa de tomate casera.', categoria: 'Platos Principales', path: '/platoPrin' },
-    { id: 5, name: 'Pollo al Ajillo', description: 'Pollo dorado con ajo y vino blanco.', categoria: 'Segundo Platos', path: '/platoSec' },
-    { id: 6, name: 'Ensalada Mixta', description: 'Una refrescante ensalada con lechuga, tomate, cebolla y atún.', categoria: 'Platos Principales', path: '/platoPrin' },
-    { id: 7, name: 'Sopa de Verduras', description: 'Una sopa reconfortante con una variedad de verduras frescas.', categoria: 'Platos Principales', path: '/platoPrin' },
-    { id: 8, name: 'Arroz con Leche', description: 'Un postre tradicional de arroz cocido en leche con canela.', categoria: 'Postres', path: '/postres' },
-    { id: 9, name: 'Helado de chocolate', description: 'Un postre refrescante y delicioso de chocolate.', categoria: 'Postres', path: '/postres' },
-    { id: 10, name: 'Crema de Calabaza', description: 'Una crema suave y dulce de calabaza con un toque de nuez moscada.', categoria: 'Platos Principales', path: '/platoPrin' },
-    { id: 11, name: 'Revuelto de Setas', description: 'Un revuelto cremoso con setas variadas y huevo.', categoria: 'Platos Principales', path: '/platoPrin' },
-    { id: 12, name: 'Steak a la Parrilla', description: 'Jugoso filete de ternera cocinado a la parrilla.', categoria: 'Segundo Platos', path: '/platoSec' },
-    { id: 13, name: 'Paella', description: 'Un clásico plato español con arroz y mariscos..', categoria: 'Segundos Plato', path: '/platoSec' },
-    { id: 14, name: 'Salmón al Horno', description: 'Salmón jugoso horneado con limón y eneldo.', categoria: 'Segundos Plato', path: '/platoSec' },
-    { id: 15, name: 'Lasaña', description: 'Un delicioso plato de pasta con carne y bechamel.', categoria: 'Segundos Plato', path: '/platoSec' },
-    { id: 16, name: 'Croquetas', description: 'Deliciosas croquetas crujientes por fuera y cremosas por dentro.', categoria: 'Entrantes', path: '/entrantes' },
-    { id: 17, name: 'Gazpacho', description: 'Una sopa fría de tomate, perfecta para los días calurosos.', categoria: 'Entrantes', path: '/entrantes' },
-    { id: 18, name: 'Patatas Bravas', description: 'Patatas fritas servidas con una salsa picante.', categoria: 'Entrantes', path: '/entrantes' },
-    { id: 19, name: 'Bruschetta', description: 'Tostadas de pan con tomate, ajo y albahaca, un clásico aperitivo italiano.', categoria: 'Entrantes', path: '/entrantes' },
-    { id: 20, name: 'Hummus', description: 'Un dip cremoso de garbanzos, perfecto para acompañar con pan pita.', categoria: 'Entrantes', path: '/entrantes' }
-    
-];
-
-const recipesCategories = [ // Array de categorías (mantenemos esto)
-    { id: 1, name: 'Entrantes', description: 'Platos ligeros y apetitosos para comenzar la comida.' },
-    { id: 2, name: 'Primer Plato', description: 'Recetas sustanciosas y completas para el eje central de la comida.' },
-    { id: 3, name: 'Segundo Plato', description: 'Opciones que complementan el plato principal.' },
-    { id: 4, name: 'Postres', description: 'Dulces y delicias para cerrar la comida.' },
-    { id: 5, name: 'PanelAdmin', description: 'Enlace al panel de administración.' },
-];
-
 const Inicio = () => {
+    const location = useLocation();
     const [searchTerm, setSearchTerm] = useState('');
-    const [filteredRecipes, setFilteredRecipes] = useState([]); // Estado para las recetas filtradas
-    const [showCategories, setShowCategories] = useState(true); // Estado para mostrar/ocultar categorías
+    const [userProfileImage, setUserProfileImage] = useState('');
+    const [recipesCategories, setRecipesCategories] = useState([]);
 
     useEffect(() => {
-        // Filtra las recetas al cargar la página (si hay un término de búsqueda)
-        if (searchTerm) {
-            handleSearchSubmit();
+        // Obtener la información del usuario
+        const fetchUserProfile = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8000/usuarios/${location.state.id_usuario}`, {
+                    headers: {
+                        'Authorization': `Bearer Reto5Niger`,
+                    },
+                });
+                const usuario = response.data;
+                const profileImage = require(`../../images/${usuario.foto_perfil}.png`); // Importar la imagen
+                setUserProfileImage(profileImage);
+            } catch (error) {
+                console.error('Error al obtener la información del usuario:', error);
+            }
+        };
+
+        if (location.state && location.state.foto_perfil) {
+            const profileImage = require(`../../images/${location.state.foto_perfil}.png`); // Importar la imagen
+            setUserProfileImage(profileImage);
+        } else {
+            fetchUserProfile();
         }
-    }, []); // Se ejecuta solo una vez al montar el componente
+
+        // Obtener las categorías de recetas
+        const fetchCategories = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/categorias', {
+                    headers: {
+                        'Authorization': `Bearer Reto5Niger`,
+                    },
+                });
+                setRecipesCategories(response.data);
+            } catch (error) {
+                console.error('Error al obtener las categorías:', error);
+            }
+        };
+
+        fetchCategories();
+    }, [location.state]);
 
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
     };
 
-    const handleSearchSubmit = () => {
-        const filtered = allRecipes.filter((recipe) => // Busca en allRecipes
-            recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setFilteredRecipes(filtered);
-        setShowCategories(false); // Oculta las categorías al buscar
-    };
-
     const handleCategoryClick = (categoryName) => {
-        // Implementa la lógica para redirigir a la página de la categoría
-        // (Esto ya lo tenías, solo lo adaptamos)
         switch (categoryName) {
             case 'Postres':
                 return '/postres';
@@ -78,22 +71,26 @@ const Inicio = () => {
             case 'PanelAdmin':
                 return '/panelAdmin';
             default:
-                return '/'; // Página de inicio por defecto
+                return '/';
         }
     };
 
     return (
         <div className="inicio-container">
             <div className="header-inicio">
-            <img src={logoImage} alt="Logo" className="logo-image" />
-            <h1>¡Bienvenido a Recetas Niger!</h1>
-            <Link to="/subirReceta">
+                <img src={logoImage} alt="Logo" className="logo-image" />
+                <h1>¡Bienvenido a Recetas Niger!</h1>
+                <Link to="/subirReceta">
                     <button className="upload-recipe-button">Subir Receta</button>
-            </Link>
+                </Link>
             </div>
             <div className="perfil">
                 <Link to="/perfil">
-                    <button className="perfil-button">Mi Perfil</button>
+                    {userProfileImage ? (
+                        <img src={userProfileImage} alt="Perfil" className="profile-image" />
+                    ) : (
+                        <button className="perfil-button">Mi Perfil</button>
+                    )}
                 </Link>
             </div>
             <div className="salir">
@@ -102,7 +99,7 @@ const Inicio = () => {
                 </Link>
             </div>
             <div>
-                <img src={Tenedor} alt="Tenedor" className="iconos icono-tenedor"/>
+                <img src={Tenedor} alt="Tenedor" className="iconos icono-tenedor" />
                 <img src={Cuchillo} alt="Cuchillo" className="iconos icono-cuchillo" />
             </div>
             <div className="search-container">
@@ -113,42 +110,22 @@ const Inicio = () => {
                     onChange={handleSearchChange}
                     className="search-input"
                 />
-                <button onClick={handleSearchSubmit} className="search-button">Buscar</button>
                 <Link to="/ingredientes">
-                <button className="search-button">Ingredientes</button>
+                    <button className="search-button">Ingredientes</button>
                 </Link>
             </div>
-            
 
-            {showCategories && (
-                <div className="recipes-list">
-                    {recipesCategories.map((category) => (
-                        <div key={category.id} className="recipe-item">
-                            <h2>{category.name}</h2>
-                            <p>{category.description}</p>
-                            <Link to={handleCategoryClick(category.name)}>
-                                <button className="view-button">Ver {category.name}</button>
-                            </Link>
-                        </div>
-                    ))}
-                </div>
-
-            )}
-            
-            {!showCategories && (
-                <div className="recipes-list">
-                    {filteredRecipes.length > 0 ? (
-                        filteredRecipes.map((recipe) => (
-                            <div key={recipe.id} className="recipe-item">
-                                <h2>{recipe.name}</h2>
-                                    <button className="view-button">Ver receta</button>
-                            </div>
-                        ))
-                    ) : (
-                        <p>No se encontraron recetas que coincidan con tu búsqueda.</p>
-                    )}
-                </div>
-            )}
+            <div className="recipes-list">
+                {recipesCategories.map((category) => (
+                    <div key={category.id} className="recipe-item">
+                        <h2>{category.nombre_categoria}</h2>
+                        <p>{category.descripcion}</p>
+                        <Link to={handleCategoryClick(category.nombre_categoria)}>
+                            <button className="view-button">Ver {category.nombre_categoria}</button>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
