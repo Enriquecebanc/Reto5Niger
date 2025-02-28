06c8376c6ee38594a462abe8053603aa137dfabd
AimarRedondo <arger0205@gmail.com>
2025-02-28 09:20:53 +0100

    comentariosssssssss

--- Frontend/nigerweb/src/pages/login/login.js
+++ Frontend/nigerweb/src/pages/login/login.js
@@ -1,36 +1,43 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import './login.css';
+import React, { useState } from 'react'; // Importa React y el hook useState para manejar el estado
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate para redirigir al usuario a diferentes rutas
+import axios from 'axios'; // Importa axios para realizar peticiones HTTP
+import './login.css'; // Importa el archivo de estilos CSS
 
+// Componente funcional Login
 const Login = ({ onLogin }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const navigate = useNavigate();
+    // Estados del componente
+    const [email, setEmail] = useState(''); // Estado para almacenar el correo electrónico
+    const [password, setPassword] = useState(''); // Estado para almacenar la contraseña
+    const [errorMessage, setErrorMessage] = useState(''); // Estado para mostrar mensajes de error
+    const navigate = useNavigate(); // Hook para la navegación
 
+    // Manejadores de eventos para los cambios en los inputs
     const handleEmailChange = (event) => {
-        setEmail(event.target.value);
+        setEmail(event.target.value); // Actualiza el estado del correo electrónico con el valor del input
     };
 
     const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
+        setPassword(event.target.value); // Actualiza el estado de la contraseña con el valor del input
     };
 
+    // Manejador de evento para el envío del formulario
     const handleSubmit = async (event) => {
-        event.preventDefault();
+        event.preventDefault(); // Evita la recarga de la página al enviar el formulario
         try {
+            // Realiza una petición GET a la API para obtener la lista de usuarios
             const response = await axios.get('http://localhost:8000/usuarios', {
                 headers: {
-                    'Authorization': `Bearer Reto5Niger`,
+                    'Authorization': `Bearer Reto5Niger`, // Envía el token de autorización en la cabecera
                 },
             });
-            const usuarios = response.data;
+            const usuarios = response.data; // Obtiene los datos de la respuesta
 
+            // Busca un usuario que coincida con el correo electrónico y la contraseña
             const usuario = usuarios.find(user => user.correo === email && user.contraseña === password);
 
+            // Si se encuentra un usuario
             if (usuario) {
-                onLogin(email, password);
+                onLogin(email, password); // Llama a la función onLogin para indicar que el usuario ha iniciado sesión
 
                 // Obtener la información del usuario
                 const userResponse = await axios.get(`http://localhost:8000/usuarios/${usuario.id_usuario}`, {
@@ -38,23 +45,25 @@ const Login = ({ onLogin }) => {
                         'Authorization': `Bearer Reto5Niger`,
                     },
                 });
-                const userInfo = userResponse.data;
+                const userInfo = userResponse.data; // Obtiene la información del usuario
 
-                // Navegar a la página de inicio con la foto de perfil
+                // Navegar a la página de inicio con la foto de perfil y el id de usuario
                 navigate('/', { state: { id_usuario: usuario.id_usuario, foto_perfil: userInfo.foto_perfil } });
             } else {
-                setErrorMessage('Correo o contraseña incorrectos.');
+                setErrorMessage('Correo o contraseña incorrectos.'); // Establece un mensaje de error
             }
         } catch (error) {
-            console.error('Error al verificar el usuario:', error);
-            setErrorMessage('Error al verificar el usuario. Por favor, inténtalo de nuevo más tarde.');
+            console.error('Error al verificar el usuario:', error); // Muestra el error en la consola
+            setErrorMessage('Error al verificar el usuario. Por favor, inténtalo de nuevo más tarde.'); // Establece un mensaje de error
         }
     };
 
+    // Manejador de evento para el botón de registro
     const handleRegister = () => {
-        navigate('/registro');
+        navigate('/registro'); // Redirige al usuario a la página de registro
     };
 
+    // Renderiza el componente
     return (
         <div className="login-container">
             <h1>Iniciar Sesión</h1>
@@ -81,6 +90,7 @@ const Login = ({ onLogin }) => {
                         required
                     />
                 </div>
+                {/* Muestra el mensaje de error si existe */}
                 {errorMessage && <p className="error-message">{errorMessage}</p>}
                 <button type="submit" className="login-button">Iniciar Sesión</button>
                 <button type="button" className="register-button" onClick={handleRegister}>Registrar</button>
@@ -92,4 +102,4 @@ const Login = ({ onLogin }) => {
     );
 };
 
-export default Login;
+export default Login; // Exporta el componente
--- Frontend/nigerweb/src/pages/perfil/perfil.js
+++ Frontend/nigerweb/src/pages/perfil/perfil.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import './perfil.css';
+import React, { useState, useEffect } from 'react'; // Importa React, useState y useEffect
+import { Link, useLocation, useNavigate } from 'react-router-dom'; // Importa componentes de react-router-dom
+import axios from 'axios'; // Importa axios para las peticiones HTTP
+import './perfil.css'; // Importa los estilos CSS
 
+// Componente funcional Perfil
 const Perfil = ({ onLogout }) => {
+    // Obtiene la ubicación actual y la función de navegación
     const location = useLocation();
     const navigate = useNavigate();
+
+    // Estado para almacenar la información del usuario
     const [user, setUser] = useState({
         id_usuario: '',
         nombre_usuario: '',
@@ -17,6 +21,7 @@ const Perfil = ({ onLogout }) => {
         respuesta_pregunta_3: ''
     });
 
+    // Estado para controlar qué campos están en modo de edición
     const [isEditing, setIsEditing] = useState({
         nombre_usuario: false,
         respuesta_pregunta_1: false,
@@ -24,50 +29,58 @@ const Perfil = ({ onLogout }) => {
         respuesta_pregunta_3: false
     });
 
+    // Estado para controlar la visibilidad del popup de selección de imagen
     const [showImagePopup, setShowImagePopup] = useState(false);
+    // Estado para almacenar la lista de imágenes disponibles
     const [availableImages, setAvailableImages] = useState([]);
+    // Estado para almacenar la imagen seleccionada en el popup
     const [selectedImage, setSelectedImage] = useState(null);
 
+    // useEffect para cargar la información del usuario y las imágenes disponibles al montar el componente
     useEffect(() => {
-        // Obtener la información del usuario
+        // Función asíncrona para obtener la información del usuario
         const fetchUserProfile = async () => {
             try {
+                // Realiza una petición GET a la API para obtener la información del usuario
                 const response = await axios.get(`http://localhost:8000/usuarios/${location.state.id_usuario}`, {
                     headers: {
-                        'Authorization': `Bearer Reto5Niger`,
+                        'Authorization': `Bearer Reto5Niger`, // Incluye el token de autorización en la cabecera
                     },
                 });
-                setUser(response.data);
+                setUser(response.data); // Actualiza el estado del usuario con la información obtenida
             } catch (error) {
                 console.error('Error al obtener la información del usuario:', error);
             }
         };
 
-        fetchUserProfile();
+        fetchUserProfile(); // Llama a la función para obtener la información del usuario
 
-        // Cargar las imágenes disponibles
+        // Carga las imágenes disponibles desde la carpeta 'images'
         const images = Array.from({ length: 17 }, (_, i) => require(`../../images/${i + 1}.png`));
-        setAvailableImages(images);
-    }, [location.state.id_usuario]);
+        setAvailableImages(images); // Actualiza el estado con las imágenes disponibles
+    }, [location.state.id_usuario]); // El efecto se ejecuta cuando cambia el id_usuario
 
+    // Función para activar el modo de edición de un campo específico
     const handleEditClick = (field) => {
         setIsEditing(prevState => ({
             ...prevState,
-            [field]: true
+            [field]: true // Activa el modo de edición para el campo especificado
         }));
     };
 
+    // Función asíncrona para guardar los cambios realizados en un campo específico
     const handleSaveClick = async (field) => {
         setIsEditing(prevState => ({
             ...prevState,
-            [field]: false
+            [field]: false // Desactiva el modo de edición para el campo especificado
         }));
 
-        // Guardar los cambios en la base de datos
+        // Guarda los cambios en la base de datos
         try {
+            // Realiza una petición PUT a la API para actualizar la información del usuario
             await axios.put(`http://localhost:8000/usuarios/${location.state.id_usuario}`, user, {
                 headers: {
-                    'Authorization': `Bearer Reto5Niger`,
+                    'Authorization': `Bearer Reto5Niger`, // Incluye el token de autorización en la cabecera
                 },
             });
         } catch (error) {
@@ -75,58 +88,69 @@ const Perfil = ({ onLogout }) => {
         }
     };
 
+    // Función para actualizar el estado del usuario al cambiar un campo de entrada
     const handleChange = (e) => {
-        const { name, value } = e.target;
+        const { name, value } = e.target; // Obtiene el nombre y el valor del campo de entrada
         setUser(prevState => ({
             ...prevState,
-            [name]: value
+            [name]: value // Actualiza el estado del usuario con el nuevo valor
         }));
     };
 
+    // Función para manejar el cambio de imagen seleccionada en el popup
     const handleImageChange = (index) => {
-        setSelectedImage(index + 1);
+        setSelectedImage(index + 1); // Actualiza el estado con el índice de la imagen seleccionada
     };
 
+    // Función asíncrona para guardar la imagen de perfil seleccionada
     const handleSaveImage = async () => {
-        if (selectedImage !== null) {
+        if (selectedImage !== null) { // Verifica que se haya seleccionado una imagen
             try {
+                // Crea un nuevo objeto de usuario con la imagen de perfil actualizada
                 const updatedUser = { ...user, foto_perfil: selectedImage };
+                // Realiza una petición PUT a la API para actualizar la información del usuario
                 await axios.put(`http://localhost:8000/usuarios/${location.state.id_usuario}`, updatedUser, {
                     headers: {
-                        'Authorization': `Bearer Reto5Niger`,
+                        'Authorization': `Bearer Reto5Niger`, // Incluye el token de autorización en la cabecera
                     },
                 });
-                setUser(updatedUser);
-                setShowImagePopup(false);
+                setUser(updatedUser); // Actualiza el estado del usuario con la nueva imagen de perfil
+                setShowImagePopup(false); // Cierra el popup de selección de imagen
             } catch (error) {
                 console.error('Error al subir la imagen de perfil:', error);
             }
         }
     };
 
+    // Función asíncrona para eliminar el usuario
     const handleDeleteUser = async () => {
         try {
+            // Realiza una petición DELETE a la API para eliminar el usuario
             await axios.delete(`http://localhost:8000/usuarios/${location.state.id_usuario}`, {
                 headers: {
-                    'Authorization': `Bearer Reto5Niger`,
+                    'Authorization': `Bearer Reto5Niger`, // Incluye el token de autorización en la cabecera
                 },
             });
-            onLogout();
-            navigate('/');
+            onLogout(); // Llama a la función de logout para limpiar la sesión
+            navigate('/'); // Redirige al usuario a la página de inicio
         } catch (error) {
             console.error('Error al borrar el usuario:', error);
         }
     };
 
+    // Renderiza el componente
     return (
         <div className="perfil-container">
             <div className="perfil-header">
+                {/* Muestra la imagen de perfil del usuario */}
                 {user.foto_perfil && (
                     <img src={require(`../../images/${user.foto_perfil}.png`)} alt="Profile" className="perfil-picture" onClick={() => setShowImagePopup(true)} />
                 )}
+                {/* Popup para seleccionar una nueva imagen de perfil */}
                 {showImagePopup && (
                     <div className="image-popup">
                         <div className="image-popup-content">
+                            {/* Muestra la lista de imágenes disponibles */}
                             {availableImages.map((src, index) => (
                                 <img
                                     key={index}
@@ -136,6 +160,7 @@ const Perfil = ({ onLogout }) => {
                                     onClick={() => handleImageChange(index)}
                                 />
                             ))}
+                            {/* Botones para guardar la imagen seleccionada o cerrar el popup */}
                             <button onClick={handleSaveImage}>Guardar</button>
                             <button onClick={() => setShowImagePopup(false)}>Cerrar</button>
                         </div>
@@ -143,34 +168,45 @@ const Perfil = ({ onLogout }) => {
                 )}
             </div>
             <div className="perfil-details">
+                {/* Muestra la información del usuario y permite editarla */}
                 <p><strong>Correo:</strong> {user.correo}</p>
                 <p><strong>Nombre:</strong>
+                    {/* Si está en modo de edición, muestra un campo de entrada y un botón para guardar */}
                     {isEditing.nombre_usuario ? 
                         <><input type="text" name="nombre_usuario" value={user.nombre_usuario} onChange={handleChange} /><button onClick={() => handleSaveClick('nombre_usuario')}>Guardar</button></> : 
-                        <>{user.nombre_usuario} <button onClick={() => handleEditClick('nombre_usuario')}>Editar</button></>}
+                        <>{user.nombre_usuario} {/* Si no está en modo de edición, muestra el valor y un botón para editar */}
+                         <button onClick={() => handleEditClick('nombre_usuario')}>Editar</button></>}
                 </p>
                 <p><strong>Respuesta 1:</strong>
+                    {/* Si está en modo de edición, muestra un campo de entrada y un botón para guardar */}
                     {isEditing.respuesta_pregunta_1 ? 
                         <><input type="text" name="respuesta_pregunta_1" value={user.respuesta_pregunta_1} onChange={handleChange} /><button onClick={() => handleSaveClick('respuesta_pregunta_1')}>Guardar</button></> : 
-                        <>{user.respuesta_pregunta_1} <button onClick={() => handleEditClick('respuesta_pregunta_1')}>Editar</button></>}
+                        <>{user.respuesta_pregunta_1} {/* Si no está en modo de edición, muestra el valor y un botón para editar */}
+                        <button onClick={() => handleEditClick('respuesta_pregunta_1')}>Editar</button></>}
                 </p>
                 <p><strong>Respuesta 2:</strong>
+                    {/* Si está en modo de edición, muestra un campo de entrada y un botón para guardar */}
                     {isEditing.respuesta_pregunta_2 ? 
                         <><input type="text" name="respuesta_pregunta_2" value={user.respuesta_pregunta_2} onChange={handleChange} /><button onClick={() => handleSaveClick('respuesta_pregunta_2')}>Guardar</button></> : 
-                        <>{user.respuesta_pregunta_2} <button onClick={() => handleEditClick('respuesta_pregunta_2')}>Editar</button></>}
+                        <>{user.respuesta_pregunta_2} {/* Si no está en modo de edición, muestra el valor y un botón para editar */}
+                         <button onClick={() => handleEditClick('respuesta_pregunta_2')}>Editar</button></>}
                 </p>
                 <p><strong>Respuesta 3:</strong>
+                    {/* Si está en modo de edición, muestra un campo de entrada y un botón para guardar */}
                     {isEditing.respuesta_pregunta_3 ? 
                         <><input type="text" name="respuesta_pregunta_3" value={user.respuesta_pregunta_3} onChange={handleChange} /><button onClick={() => handleSaveClick('respuesta_pregunta_3')}>Guardar</button></> : 
-                        <>{user.respuesta_pregunta_3} <button onClick={() => handleEditClick('respuesta_pregunta_3')}>Editar</button></>}
+                        <>{user.respuesta_pregunta_3} {/* Si no está en modo de edición, muestra el valor y un botón para editar */}
+                         <button onClick={() => handleEditClick('respuesta_pregunta_3')}>Editar</button></>}
                 </p>
             </div>
+            {/* Enlace para volver a la página de inicio */}
             <Link to="/" state={{ id_usuario: user.id_usuario }}>
                 <button className="back-button">Volver a Inicio</button>
             </Link>
+            {/* Botón para eliminar el usuario */}
             <button className="delete-button" onClick={handleDeleteUser}>Borrar Usuario</button>
         </div>
     );
 };
 
-export default Perfil;
+export default Perfil; // Exporta el componente
