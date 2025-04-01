9e9ab37b0c7fd4321a16881fb3a3f5ab76499262
Gari <garikworkaddress@gmail.com>
2025-04-01 12:33:06 +0200

    Agregado botón de vuelta a inicio en la página de Rss.

--- Frontend/nigerweb/src/pages/RSS/Rss.js
+++ Frontend/nigerweb/src/pages/RSS/Rss.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom"; // Importar useLocation para obtener el idUsuario
 import "./Rss.css";
 
 const RSSFeed = () => {
   const [feed, setFeed] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [idUsuario, setIdUsuario] = useState(null); // Estado para almacenar el idUsuario
+  const location = useLocation(); // Obtener la ubicación actual
+
+  useEffect(() => {
+    // Obtener el idUsuario desde el estado de la ubicación
+    if (location.state && location.state.id_usuario) {
+      setIdUsuario(location.state.id_usuario);
+    }
+  }, [location]);
 
   useEffect(() => {
     const fetchFeed = async () => {
@@ -22,11 +32,11 @@ const RSSFeed = () => {
         const items = xmlDoc.querySelectorAll("item");
 
         const parsedFeed = Array.from(items).map((item) => ({
-          title: item.querySelector("title")?.textContent || "Sin tÃ­tulo",
+          title: item.querySelector("title")?.textContent || "Sin título",
           pubDate:
             item.querySelector("updated")?.textContent || "Fecha desconocida",
           contentSnippet:
-            item.querySelector("summary")?.textContent || "Sin descripciÃ³n",
+            item.querySelector("summary")?.textContent || "Sin descripción",
           link: item.querySelector("link")?.textContent || "#",
         }));
 
@@ -43,6 +53,12 @@ const RSSFeed = () => {
   return (
     <div className="container">
       <h1 className="header">La Cocina Loca</h1>
+      <div className="contenedor-volver-inicio">
+        {/* Pasar el idUsuario al estado del Link */}
+        <Link to="/" state={{ id_usuario: idUsuario }}>
+          <button className="back-to-home-btn">Volver a Inicio</button>
+        </Link>
+      </div>
       {loading ? (
         <p>Cargando...</p>
       ) : (
@@ -54,7 +70,7 @@ const RSSFeed = () => {
                 <p className="card-date">{item.pubDate}</p>
                 <p className="card-snippet">{item.contentSnippet}</p>
                 <a href={item.link} target="_blank" rel="noopener noreferrer">
-                  <button className="read-more-btn">Leer mÃ¡s</button>
+                  <button className="read-more-btn">Leer más</button>
                 </a>
               </div>
             </div>
